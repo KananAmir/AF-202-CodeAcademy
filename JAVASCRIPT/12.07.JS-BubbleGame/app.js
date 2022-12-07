82d831be98724a08c4ef095009e79dd511f1f834
Kanan Amirov <amirovknn@gmail.com>
2022-12-07 21:06:52 +0400

    bubble game

---
+++ JAVASCRIPT/12.07.JS-BubbleGame/app.js
@@ -0,0 +1,73 @@
+let startBtn = document.querySelector("#start");
+let stopBtn = document.querySelector("#stop");
+let reset = document.querySelector("#reset");
+let box = document.querySelector(".box");
+let result = document.querySelector("#score");
+
+let bubble;
+let bubbleInterval;
+let userScore = 0;
+startBtn.addEventListener("click", function () {
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+  //   clearInterval(bubbleInterval);
+  bubbleInterval = setInterval(function () {
+    createBubble();
+  }, 500);
+
+  document.querySelectorAll(".bubble").forEach((element) => {
+    element.disabled = false;
+  });
+});
+
+stopBtn.addEventListener("click", function () {
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+  clearInterval(bubbleInterval);
+  document.querySelectorAll(".bubble").forEach((element) => {
+    element.disabled = true;
+  });
+});
+
+reset.addEventListener("click", resetGame);
+
+function resetGame() {
+  startBtn.disabled = false;
+  stopBtn.disabled = false;
+  clearInterval(bubbleInterval);
+  box.innerHTML = "";
+  userScore = 0;
+  result.innerText = userScore;
+}
+
+function createBubble() {
+  let leftPos = Math.floor(Math.random() * (95 - 1) + 1);
+  let topPos = Math.floor(Math.random() * (95 - 1) + 1);
+  bubble = document.createElement("button");
+  bubble.setAttribute("class", "bubble");
+  bubble.style.width = "25px";
+  bubble.style.height = "25px";
+  bubble.style.borderRadius = "50%";
+  bubble.style.backgroundColor = "teal";
+  bubble.style.border = "1px solid black";
+  bubble.style.cursor = "pointer";
+  bubble.style.position = "absolute";
+  bubble.style.left = `${leftPos}%`;
+  bubble.style.top = `${topPos}%`;
+
+  box.appendChild(bubble);
+
+  if (document.querySelectorAll(".bubble").length == 10) {
+    clearInterval(bubbleInterval);
+    if (confirm(`Game Over!! Your score is ${userScore}`)) {
+      resetGame();
+    }
+  }
+
+  bubble.addEventListener("click", function () {
+    this.remove();
+    userScore += 1;
+    result.innerText = userScore;
+    // bubble.style.display = "none";
+  });
+}
