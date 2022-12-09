2b309ddb3a5f8fa2bb88c5cfcbe35d956139dc23
Kanan Amirov <amirovknn@gmail.com>
2022-12-09 20:06:31 +0400

    practice

--- JAVASCRIPT/12.09.Practice/app.js
+++ JAVASCRIPT/12.09.Practice/app.js
@@ -17,9 +17,9 @@ let users = [
 ];
 
 let fitleredUsers = users.filter((element) => element.age >= 50);
-console.log(fitleredUsers);
+// console.log(fitleredUsers);
 
-console.log(users.find((q) => q.age > 50));
+// console.log(users.find((q) => q.age > 50));
 
 // console.log(users.sort((a, b) => a.age - b.age));
 // console.log(users.sort((a, b) => b.age - a.age));
@@ -43,11 +43,40 @@ let user = {
 // let userCity = user.city;
 
 let { surname, city } = user;
-console.log(surname);
-console.log(city);
+// console.log(surname);
+// console.log(city);
 
-let students = ["ali", "gulara", "emil", "izzat",'ilqar'];
+let students = ["ali", "gulara", "emil", "izzat", "ilqar"];
 
 let [first, second, ...qalanlar] = students;
 
-console.log(qalanlar);
+// console.log(qalanlar);
+
+let results = [95, 90, 100, 75, 100, 75, 100, 60, 95];
+
+// let mySetObj = new Set(results);
+
+// console.log(mySetObj);
+
+let newResults = [...new Set(results)];
+
+// console.log(newResults);
+
+let obj = {
+  username: "KananAmir",
+  email: "kanan@gmail.com",
+  pw: "kanan123",
+};
+
+let myMap = new Map();
+
+myMap.set(5, "test");
+myMap.set([1, 2], "test");
+myMap.set([1, 2, 3], [0, 5, 8]);
+myMap.set("email", "kanan@gmail.com");
+
+console.log(myMap);
+
+console.log(myMap.get(5));
+
+
