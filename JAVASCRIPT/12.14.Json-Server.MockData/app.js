04831f2471d78d073e6ffb9a87d11818605ba6a9
Kanan Amirov <amirovknn@gmail.com>
2022-12-14 14:29:41 +0400

    json server

---
+++ JAVASCRIPT/12.14.Json-Server.MockData/app.js
@@ -0,0 +1,16 @@
+// fetch("http://localhost:8000/blogs")
+//   .then((response) => response.json())
+//   .then((data) => console.log(data));
+
+
+// fetch("http://localhost:8000/blogs/", {
+//   method: "POST",
+//   headers: {
+//     "content-type": "application/json",
+//   },
+//   body: JSON.stringify({
+//     title: "Quam id voluptate id",
+//     body: "Numquam proident re",
+//     author: "kanan",
+//   }),
+// });
